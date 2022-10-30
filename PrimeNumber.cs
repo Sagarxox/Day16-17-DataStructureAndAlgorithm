cf140d6c132d76eaf35142f428626767cf624bb8
sagar <skatahre@gmail.com>
2022-10-30 16:42:16 +0530

    program to find prime number

---
+++ PrimeNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16_17_DSA
+{
+    internal class PrimeNumber
+    {
+      
+            static void Main(string[] args)
+            {
+                bool isPrime = true;
+                Console.WriteLine("Prime Numbers in Range Of 0-1000: ");
+                for (int i = 2; i <= 1000; i++)
+                {
+                    for (int j = 2; j <= 1000; j++)
+                    {
+
+                        if (i != j && i % j == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+
+                    }
+                    if (isPrime)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    isPrime = true;
+                }
+
+            }
+        
+    }
+}
