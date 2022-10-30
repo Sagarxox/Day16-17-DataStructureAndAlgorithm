d2a002bd623fb3b7be347a66aeedf0ba65b18f3e
sagar <skatahre@gmail.com>
2022-10-30 16:24:03 +0530

    welcome message

---
+++ master.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Day16_17_DSA
+{
+    public class master
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to DSA practice programs");
+        }
+    }
+}
