39fea5be5d860f6683261d719d7f2307289335b8
sagar <skatahre@gmail.com>
2022-10-30 16:25:48 +0530

    bubble sort program

---
+++ BubbleSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16_17_DSA
+{
+    public class BubbleSort
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 70, 80, 35, 94, 21 };
+            int temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Bubble Sorted:");
+            foreach (int p in arr)
+                Console.Write(p + " ");
+            Console.Read();
+        }
+    }
+}
+
